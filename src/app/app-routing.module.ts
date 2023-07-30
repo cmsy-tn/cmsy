cb7acaf50f8bdb9c198d8c54e4e94fec844880bd
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-07-31 00:49:41 +0100

    Routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// *** CUSTOM IMPORTS *** \\
+import { HomeComponent } from './views/home/home.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { SettingsComponent } from './views/settings/settings.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: "full", component: HomeComponent },
+  { path: '@me', component: ProfileComponent },
+  { path: 'settings', component: SettingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
