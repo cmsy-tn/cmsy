302c021bc821459ba290c6aa33569113027ab11e
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-08 19:12:50 +0100

    standard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// CUSTOM IMPORTS
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+
 const routes: Routes = [
-  // { path: '', pathMatch: "full", component: HomeComponent },
-  // { path: '@me', component: ProfileComponent },
-  // { path: 'settings', component: SettingsComponent },
+  { path: '', pathMatch: "full", component: DashboardComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
