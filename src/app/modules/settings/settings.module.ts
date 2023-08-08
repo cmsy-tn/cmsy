7c24c64c17fd95de0af0f6cb635d767e3f50a757
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-08 20:28:08 +0100

    boostrapped the Settings module routing

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SettingsRoutingModule } from './settings-routing.module';
+
+// CUSTOM VIEWS & COMPONENTS
+import { IndexComponent } from './views/index/index.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SettingsRoutingModule
   ]
 })
 export class SettingsModule { }
