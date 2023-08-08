0c83194480513bdaa184e22ac49c1ac3407c7f4a
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-08 20:21:56 +0100

    unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 // CUSTOM IMPORTS
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
