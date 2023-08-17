b2721f7a4b359032098a58bdb2245c1f049e740a
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-17 19:12:15 +0100

    boilderplate code for FAQ service

--- src/app/modules/faq/faq.service.ts
+++ src/app/modules/faq/faq.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import FAQTYPE from 'src/app/types/faq';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FaqService {
 
-  constructor() { }
+  API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  addElement(faq: FAQTYPE) {
+    return this.http.post(`${this.API_URL}/faq/`, faq);
+  }
+
+  getElements(): any {
+    return this.http.get(`${this.API_URL}/faq`);
+  }
+
+  getElement(faq_id: string): any {
+    return this.http.get(`${this.API_URL}/faq/${faq_id}`);
+  }
+
+  deleteElement(faq_id: string): any {
+    return this.http.delete(`${this.API_URL}/faq/${faq_id}`);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
     auth0: {
         domain: 'dev-blucf6ji.us.auth0.com',
         clientId: 'jxL7pDleEaChsAUn9uzGoi3QaYTFviNm',
-    }
+    },
+    API_URL: 'http://localhost;3000/'
 };
