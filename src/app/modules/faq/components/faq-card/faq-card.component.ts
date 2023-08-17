b7d06757009a514a8a5f11efb8d3aa73fde92d99
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-17 20:19:52 +0100

    FAQ dekete logic placeholder

--- src/app/modules/faq/components/faq-card/faq-card.component.ts
+++ src/app/modules/faq/components/faq-card/faq-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FaqService } from '../../faq.service';
 
 @Component({
   selector: 'cmsy-faq-card',
@@ -7,4 +8,14 @@ import { Component, Input } from '@angular/core';
 })
 export class FaqCardComponent {
   @Input() data: any;
+
+  constructor(private faqService: FaqService) { }
+
+  deleteFAQ(faq_id: string) {
+    this.faqService.deleteElement(faq_id).subscribe({
+      next: (response: any[]) => {
+        // TRIGGER TOASTER FOR SUCCESS !
+      }
+    })
+  }
 }
