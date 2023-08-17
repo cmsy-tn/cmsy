55738382f48e5767efc386f93e736bfdaf327ab7
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-17 18:44:11 +0100

    Importing formsModule to be used in FAQ section

--- src/app/modules/faq/faq.module.ts
+++ src/app/modules/faq/faq.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaqRoutingModule } from './faq-routing.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // CUSTOM VIEWS & COMPONENTS
 import { IndexComponent } from './views/index/index.component';
 import { FaqCardComponent } from './components/faq-card/faq-card.component';
@@ -17,7 +17,9 @@ import { SharedModule } from "../shared/shared/shared.module";
   imports: [
     CommonModule,
     FaqRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class FaqModule { }
