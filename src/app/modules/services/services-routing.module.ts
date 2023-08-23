ec734a2e9bb6824d7420c0b1d94600453976c2d9
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-23 18:30:18 +0100

    Service module default view

--- src/app/modules/services/services-routing.module.ts
+++ src/app/modules/services/services-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './views/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/services/services.module.ts
+++ src/app/modules/services/services.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ServicesRoutingModule } from './services-routing.module';
+import { IndexComponent } from './views/index/index.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent
+  ],
   imports: [
     CommonModule,
     ServicesRoutingModule
