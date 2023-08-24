9e47afbdfe125767a18918a660bf91ce951a04e5
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-24 18:36:01 +0100

    services service CRD operations + behaviorSubject for local state management

--- src/app/modules/services/service.service.ts
+++ src/app/modules/services/service.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { SERVICEACTIONSTATE, SERVICETYPE } from 'src/app/types/service.type';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  API_URL = environment.API_URL;
+  DATA_IS_BEING_SENT = new BehaviorSubject(false);
+  FAQ_HAS_BEEN_TRIGGERED$ = new BehaviorSubject<SERVICEACTIONSTATE>(<SERVICEACTIONSTATE>{ state: false, id: '', action: '' });
+
+  constructor(private http: HttpClient) { }
+
+  addElement(faq: SERVICETYPE) {
+    return this.http.post(`${this.API_URL}/services/`, faq);
+  }
+
+  getElements(): any {
+    return this.http.get(`${this.API_URL}/services`);
+  }
+
+  getElement(faq_id: string): any {
+    return this.http.get(`${this.API_URL}/services/${faq_id}`);
+  }
+
+  // updateElement(faq_id: string, payload: UPDATED_FAQ) {
+  //   return this.http.put(`${this.API_URL}/services/${faq_id}`, payload);
+  // }
+
+  deleteElement(faq_id: string): any {
+    return this.http.delete(`${this.API_URL}/services/${faq_id}`);
+  }
 }
