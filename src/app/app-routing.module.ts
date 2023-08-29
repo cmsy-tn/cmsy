c92f25f35358d279bd1d075cb67164eb8ed6f895
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-29 09:40:31 +0100

    ng-zorro boilerplate cleanup to match old config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// CUSTOM IMPORTS
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'faq', loadChildren: () => import("src/app/modules/faq/faq.module").then((module) => module.FaqModule) },
+  { path: 'services', loadChildren: () => import("src/app/modules/services/services.module").then((module) => module.ServicesModule) },
+  { path: 'settings', loadChildren: () => import("src/app/modules/settings/settings.module").then((module) => module.SettingsModule) },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
