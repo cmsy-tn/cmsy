bd089e6c32d50ad26faeecb25fffaaaee20b0751
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-29 09:47:43 +0100

    AppModule cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { HttpClientModule } from '@angular/common/http';
-// Import the module from the SDK
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+// Auth0
 import { AuthModule } from '@auth0/auth0-angular';
-
+// CUSTOM MODULES
+import { NgZorroModule } from './ng-zorro.module';
 // CUSTOM IMPORTS
 import { AuthenticationAlertComponent } from './views/authentication-alert/authentication-alert.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
@@ -36,7 +35,6 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // Import the module into the application, with configuration
     AuthModule.forRoot({
       domain: environment.auth0.domain,
       clientId: environment.auth0.clientId,
@@ -44,7 +42,8 @@ registerLocaleData(en);
         redirect_uri: window.location.origin
       }
     }),
-    HttpClientModule
+    HttpClientModule,
+    NgZorroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
