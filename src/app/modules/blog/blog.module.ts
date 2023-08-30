c6cfcfd2957cd1158be026118d7721ac98659f7e
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-31 00:39:10 +0100

    add-one blog view boilerplate

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -4,12 +4,14 @@ import { NgZorroModule } from 'src/app/ng-zorro.module';
 import { SharedModule } from '../shared/shared.module';
 import { BlogRoutingModule } from './blog-routing.module';
 import { IndexComponent } from './views/index/index.component';
+import { BlogAddComponent } from './views/blog-add/blog-add.component';
 
 
 
 @NgModule({
   declarations: [
-    IndexComponent
+    IndexComponent,
+    BlogAddComponent
   ],
   imports: [
     CommonModule,
