88584d7ec532e088086e999853ac2a2e226958e9
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-31 02:49:18 +0100

    Blog post | save / update logic

--- src/app/modules/blog/views/blog-add/blog-add.component.ts
+++ src/app/modules/blog/views/blog-add/blog-add.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BlogPostType, STATUS_ENUM } from 'src/app/types/blog.post.type';
 import { BlogService } from '../../blog.service';
 
@@ -7,7 +8,7 @@ import { BlogService } from '../../blog.service';
   templateUrl: './blog-add.component.html',
   styleUrls: ['./blog-add.component.scss']
 })
-export class BlogAddComponent {
+export class BlogAddComponent implements OnInit {
   // DUMMY POST USED TO KEEP STATE
   // WHY DIDN'T I USEA FORM I HEAR YOU ASK
   // Raised an issue 🙌
@@ -18,7 +19,26 @@ export class BlogAddComponent {
     post_cover_image: ''
   }
 
-  constructor(private blogService: BlogService) { }
+  // USED TO KEEP TRACK OF CURRENT MODE AND LOADED POST
+  EDITOR_MODE = {
+    current_id: '',
+    current_action: ''
+  }
+
+  constructor(
+    private blogService: BlogService,
+    private currentRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.currentRoute.queryParams.subscribe({
+      next: (value: any) => { this.EDITOR_MODE.current_action = value.action; }
+    })
+    this.currentRoute.params.subscribe({
+      next: (value: any) => { this.EDITOR_MODE.current_id = value.id; }
+    })
+  }
 
   savePost(STATUS: string) {
     // making sure the post always has a title
@@ -26,10 +46,27 @@ export class BlogAddComponent {
       ...this.BlogPost,
       post_status: (this.BlogPost.post_title !== '' && STATUS === 'valid') ? STATUS_ENUM.VALID : STATUS_ENUM.DRAFT
     }
-    this.blogService.addElement(POST_TO_SAVE).subscribe({
-      next: (response: any) => {
-        alert('Post has been saved!');
-      }
-    })
+    if (this.EDITOR_MODE.current_action === 'write')
+      this.blogService.addElement(POST_TO_SAVE).subscribe({
+        next: (response: any) => {
+          this.handlePostSubmit(response.id);
+        }
+      })
+    if (this.EDITOR_MODE.current_action === 'update')
+      this.blogService.update(this.EDITOR_MODE.current_id, POST_TO_SAVE).subscribe({
+        next: (response: any) => {
+          alert('Post has been updated!')
+        }
+      })
+  }
+
+  handlePostSubmit(id: string) {
+    alert('Post has been saved!');
+    // post saved but current mode is still write
+    //    👉️ saving will result in creating a new entry in DB
+    // navigate baack to same url but with updated params (id + mode)
+    //    👉️ saving will result in updating current entry in DB
+    this.router.navigate(['/blog/' + id], { queryParams: { action: 'update' } })
+    // now checl for current mode before saving to db
   }
 }
