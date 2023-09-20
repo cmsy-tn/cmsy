d7d55c8edca448cd312358113faabd730619c4f5
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-09-20 22:23:48 +0100

    [WIP] Profile page | form

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { group } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'cmsy-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  userDataForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.buildUserForm();
+  }
+
+  buildUserForm() {
+    this.userDataForm = this.formBuilder.group({
+      user_full_name: [null, Validators.required],
+      user_bio: [null, Validators.required],
+      user_email: [null, Validators.required],
+    })
+  }
+
+  save() {
+    /**
+     * Get user data from Database using the authenticated ID
+     * if user data
+     *    👉️ update
+     *    👉️ create new
+     */
+  }
 }
